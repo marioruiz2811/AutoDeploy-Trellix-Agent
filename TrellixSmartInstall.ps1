d786b090cd6de0d97cefe0dc2eddca94882c8abb
mr124816 <71531721+marioruiz2811@users.noreply.github.com>
2024-04-26 14:31:40 -0600

    Create TrellixSmartInstall.ps1

---
+++ TrellixSmartInstall.ps1
@@ -0,0 +1,83 @@
+param(
+  [string]$Tenant,
+  [string]$Token
+)
+Function Start ([string]$Tenant,[string]$Token)
+{
+    Try
+    {
+        If ( ( $Tenant -eq "" ) -Or ( $Token -eq "" ) )
+        {
+         Exit 1;
+        }
+        else
+        {
+            $Installer = "$env:ProgramData\TrellixSmartInstaller.exe"
+            [URI]$API_URL = "https://" + $Tenant +"/ComputerMgmt/agentPackageDownload/TrellixSmartInstall.download?token=" + $Token;
+            $TrellixAgentInstalled = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Trellix Agent" };
+            $McAfeeAgentInstalled = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "McAfee Agent" };
+            If ( -Not ( $TrellixAgentInstalled.Name -eq "Trellix Agent" ) -And -Not ( $McAfeeAgentInstalled.Name -eq "McAfee Agent" ))
+            {
+                for ($i=0; $i -lt 5; $i++)
+                {
+                    If ( Test_Trellix_Connection $API_URL.Host -eq $True)
+                    {
+                        If ( Test-Path -Path $TrellixInstaller -PathType Leaf )
+                        {
+                            Remove-Item $TrellixInstaller -Force;
+                        }
+                        Start-BitsTransfer -Source $TrellixURL -Destination $TrellixInstaller;
+
+                        If (Test-Path -Path $TrellixInstaller -PathType Leaf )
+                        {
+                            If ( Test-Path -Path $TrellixInstaller -PathType Leaf )
+                            {
+                                &  $TrellixInstaller -s -f;
+                                Exit 0;
+                            }
+                        }
+                        Break;
+                    }
+                    else
+                    {
+                        sleep -Seconds 10;
+                    }
+                }
+                Exit 0;
+            }
+            else
+            {
+                If ( Test-Path -Path $Installer -PathType Leaf )
+                {
+                    Remove-Item $Installer -Force;
+                    Exit 0;
+                }
+            }
+        }
+    }
+    Catch
+    {
+     Exit 1;
+    }
+}
+
+Function Test_Trellix_Connection ([string]$TrellixHost)
+{
+    Try
+    {
+        If (-Not (Test-Connection $TrellixHost -Count 2 -Delay 2 -Quiet))
+        {
+            Return $False;
+        }
+        else
+        {
+            Return $True;
+        }
+    }
+    Catch
+    {
+        Exit 1;
+    }
+}
+
+Start "$Tenant" "$Token"
